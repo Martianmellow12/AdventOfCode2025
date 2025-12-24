7b714f665d5c3b6d3108cdc9a774b0803723c7f2
Michael <martianmellow12@gmail.com>
2025-12-23 20:00:46 -0500

    Completed part 1 of day 2

---
+++ day_2/src/main.rs
@@ -0,0 +1,70 @@
+use std::{env, fs};
+
+#[derive(Debug)]
+struct Range {
+    lower: u64,
+    upper: u64,
+}
+
+impl Range {
+    fn is_invalid_pt1(num: u64) -> bool {
+        let num_str = num.to_string();
+        
+        // No odd numbers of digits
+        if (num_str.len() % 2) == 1 {
+            return false;
+        }
+
+        // Split the number in half and compare
+        let split_point = num_str.len()/2;
+        let a = &num_str[0..split_point];
+        let b = &num_str[split_point..];
+
+        a == b
+    }
+
+    fn get_invalid_pt1(&self) -> Vec<u64> {
+        let mut result: Vec<u64> = Vec::new();
+        for id in self.lower..=self.upper {
+            if Range::is_invalid_pt1(id) {
+                result.push(id);
+            }
+        }
+        result
+    }
+
+    fn is_invalid_pt2(num: u64) -> bool {
+        
+    }
+}
+
+fn main() {
+    // Load the input
+    let mut ranges: Vec<Range> = Vec::new();
+    let data = fs::read_to_string("input.txt").unwrap();
+    let range_strs: Vec<&str> = data.split(",").collect();
+    for i in range_strs.iter() {
+        let range_pair: Vec<&str> = i.split("-").collect();
+        let range = Range {
+            lower: range_pair.get(0)
+                             .unwrap()
+                             .to_string()
+                             .parse::<u64>()
+                             .unwrap(),
+            upper: range_pair.get(1)
+                             .unwrap()
+                             .to_string()
+                             .parse::<u64>()
+                             .unwrap(),
+        };
+        ranges.push(range);
+    }
+
+    // Part 1
+    let mut invalid_ids: Vec<u64> = Vec::new();
+    for range in ranges.iter() {
+        invalid_ids.append(&mut range.get_invalid_pt1());
+    }
+    let sum: u64 = invalid_ids.iter().sum();
+    println!("Part 1: {:?}", sum);
+}
